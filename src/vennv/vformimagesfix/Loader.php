5425fd5bfc54953715049373ffd2ba63b8138e7f
VennV <111500380+VennDev@users.noreply.github.com>
2023-07-30 00:59:48 +0700

    Add files via upload

---
+++ src/vennv/vformimagesfix/Loader.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * VFormImagesFix - PocketMine plugin.
+ * Copyright (C) 2023 - 2025 VennDev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace vennv\vformimagesfix;
+
+use pocketmine\entity\Attribute;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\DataPacket;
+use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
+use pocketmine\network\mcpe\protocol\types\entity\Attribute as NetworkAttribute;
+use pocketmine\network\mcpe\protocol\UpdateAttributesPacket;
+use vennv\vapm\Async;
+use vennv\vapm\Promise;
+use vennv\vapm\VapmPMMP;
+use Throwable;
+
+final class Loader extends PluginBase implements Listener
+{
+
+    private const PACKETS_TO_SEND = 5;
+
+    protected function onEnable(): void
+    {
+        VapmPMMP::init($this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onDataPacketSend(DataPacketSendEvent $event): void
+    {
+        $packets = $event->getPackets();
+        $targets = $event->getTargets();
+
+        array_map (
+            /** @throws Throwable */
+            function (DataPacket $packet) use ($targets) {
+                if ($packet instanceof ModalFormRequestPacket) {
+                    foreach ($targets as $target) {
+
+                        $player = $target->getPlayer();
+
+                        if ($player !== null && $player->isOnline()) {
+
+                            // Async Await to handle too many packets being sent at one time.
+                            new Async(function() use ($player, $target) {
+                                for ($i = 0; $i < self::PACKETS_TO_SEND; ++$i) {
+                                    Async::await(new Promise(function($resolve) use ($player, $target) {
+                                        if ($target->isConnected()) {
+                                            $attribute = $player->getAttributeMap()->get(Attribute::EXPERIENCE_LEVEL);
+
+                                            $id = $attribute->getId();
+                                            $minValue = $attribute->getMinValue();
+                                            $maxValue = $attribute->getMaxValue();
+                                            $value = $attribute->getValue();
+                                            $defaultValue = $attribute->getDefaultValue();
+
+                                            $networkAttribute = new NetworkAttribute($id, $minValue, $maxValue, $value, $defaultValue, []);
+
+                                            $updateAttributePacket = UpdateAttributesPacket::create($player->getId(), [$networkAttribute], 0);
+
+                                            $target->sendDataPacket($updateAttributePacket);
+                                        }
+
+                                        $resolve();
+                                    }));
+                                }
+                            });
+                        }
+                    }
+                }
+            }, $packets
+        );
+    }
+
+}
